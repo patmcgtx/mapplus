d0e0818bf1aead20c0a562b8729efa064766a63e
patmcgtx <57506933+patmcgtx@users.noreply.github.com>
2025-10-19 11:35:41 -0500

    Clarified tests

--- MapPlus/MapPlusTests/LandmarkTests.swift
+++ MapPlus/MapPlusTests/LandmarkTests.swift
@@ -5,31 +5,34 @@ import MapKit
 struct LandmarkTests {
     
     @Test func testInitialization() {
-        let coordinate = CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194)
-        let landmark = Landmark(name: "Golden Gate", systemImageName: "bridge", location: coordinate)
         
-        #expect(landmark.name == "Golden Gate")
+        let coordinate = CLLocationCoordinate2D(latitude: 37.81985, longitude: -122.47852)
+        let landmark = Landmark(name: "Golden Gate Bridge", systemImageName: "bridge", location: coordinate)
+        
+        #expect(landmark.name == "Golden Gate Bridge")
         #expect(landmark.systemImageName == "bridge")
         #expect(landmark.location.latitude == coordinate.latitude)
         #expect(landmark.location.longitude == coordinate.longitude)
     }
-    
-    @Test func testUniqueness() {
-        let coordinate = CLLocationCoordinate2D(latitude: 30.0, longitude: -97.0)
-        let landmark1 = Landmark(name: "Domain", systemImageName: "storefront", location: coordinate)
-        let landmark2 = Landmark(name: "Another", systemImageName: "circle", location: coordinate)
-        
-        // While different instances, uniqueness in persistence should be based on location.
-        #expect(landmark1.location.latitude == landmark2.location.latitude)
-        #expect(landmark1.location.longitude == landmark2.location.longitude)
-    }
-    
+
     @Test func testCoordinateComputedProperty() {
+        
         let lat: CLLocationDegrees = 35.0
         let lon: CLLocationDegrees = -120.0
-        let landmark = Landmark(name: "Test", systemImageName: "star", location: .init(latitude: lat, longitude: lon))
+        let landmark = Landmark(name: "Some landmark", systemImageName: "microphone", location: .init(latitude: lat, longitude: lon))
         
         #expect(landmark.location.latitude == lat)
         #expect(landmark.location.longitude == lon)
+    }
+    
+    @Test func testUniqueness() {
+        
+        let coordinate = CLLocationCoordinate2D(latitude: 30.0, longitude: -97.0)
+        let landmark = Landmark(name: "A Landmark", systemImageName: "mappin", location: coordinate)
+        let samePlace = Landmark(name: "Same Landmark", systemImageName: "mappin.circle", location: coordinate)
+        
+        // Uniqueness is based entirely on location (other attribues become an upsert in SwiftData)
+        #expect(landmark.location.latitude == samePlace.location.latitude)
+        #expect(landmark.location.longitude == samePlace.location.longitude)
     }
 }
