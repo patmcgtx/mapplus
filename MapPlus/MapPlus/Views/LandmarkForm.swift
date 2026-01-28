b26800bd47a08e53eccca9c0d4bfdd64e107a395
patmcgtx <57506933+patmcgtx@users.noreply.github.com>
2026-01-27 20:01:11 -0600

    #6 Initial interaction from landmarks list

---
+++ MapPlus/MapPlus/Views/LandmarkForm.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkForm.swift
+//  MapPlus
+//
+//  Created by Patrick McGonigle on 1/27/26.
+//
+
+import SwiftUI
+
+struct LandmarkForm: View {
+    var body: some View {
+        Form {
+            Text("Hello, World!")
+        }
+    }
+}
+
+#Preview {
+    LandmarkForm()
+}
