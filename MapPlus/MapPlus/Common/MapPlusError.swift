18aff9b9a881063c666bba7b4e26f0a76e643d5b
patmcgtx <57506933+patmcgtx@users.noreply.github.com>
2026-02-01 15:35:32 -0600

    #6 Added some docs (thanks, AI)

--- MapPlus/MapPlus/Common/MapPlusError.swift
+++ MapPlus/MapPlus/Common/MapPlusError.swift
@@ -5,12 +5,18 @@
 //  Created by Patrick McGonigle on 1/31/26.
 //
 
+/// Errors used by the MapPlus module.
+///  This enum represents the different error conditions that can occur
+///  when performing map-related lookups and operations within MapPlus.
 enum MapPlusError: Error {
     
+    /// Indicates that a lookup (for example, reverse geocoding or address parsing)
+    /// returned no address for the provided input.
     case noAddressFound
     
-    // TODO patmcg localize
+    /// A human-readable description of the error.
     var errorMessage: String {
+        // TODO patmcg localize
         switch self {
         case .noAddressFound:
             return "No address found"
