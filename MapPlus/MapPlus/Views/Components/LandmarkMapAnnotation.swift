88d55c80247f4c61ee665b3bec59bcb52f2fec51
patmcgtx <57506933+patmcgtx@users.noreply.github.com>
2026-03-19 14:07:21 -0500

    #47 Added map annotation previews with each theme

--- MapPlus/MapPlus/Views/Components/LandmarkMapAnnotation.swift
+++ MapPlus/MapPlus/Views/Components/LandmarkMapAnnotation.swift
@@ -27,12 +27,45 @@ struct LandmarkMapAnnotation: View {
     }
 }
 
-#Preview {
-    VStack {
-        LandmarkMapAnnotation(emoji: "📍")
-        LandmarkMapAnnotation(emoji: "🇺🇸")
-        LandmarkMapAnnotation(emoji: "🐢☀️")
+// MARK: - Previews
+
+private struct AnnotationPreview: View {
+    
+    let landmarkEmoji: String
+    
+    var body: some View {
+        ForEach(MapPlusTheme.allCases) { theme in
+            HStack{
+                Text(theme.localizedName)
+                LandmarkMapAnnotation(emoji: landmarkEmoji)
+                    .apply(theme: theme)
+            }
+        }
     }
-    .padding(40)
-    .background(.green).cornerRadius(10)
+}
+
+#Preview("Coffee") {
+    AnnotationPreview(landmarkEmoji: "☕️")
+}
+
+#Preview("Many") {
+    AnnotationPreview(landmarkEmoji: "🐢🦞📚")
+}
+
+#Preview("Initials") {
+    AnnotationPreview(landmarkEmoji: "PM")
+}
+
+#Preview("Blue") {
+    AnnotationPreview(landmarkEmoji: "PM")
+            .padding(40)
+            .background(.blue)
+            .cornerRadius(10)
+}
+
+#Preview("Green") {
+    AnnotationPreview(landmarkEmoji: "PM")
+            .padding(40)
+            .background(.green)
+            .cornerRadius(10)
 }
