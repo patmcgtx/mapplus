2beaa4e6e00b90f4f7ceee50cd4113b29fb42179
patmcgtx <57506933+patmcgtx@users.noreply.github.com>
2026-04-18 11:52:56 -0500

    #138 Cleanup

--- MapPlus/MapPlus/Views/CategoriesViewFlow.swift
+++ MapPlus/MapPlus/Views/CategoriesViewFlow.swift
@@ -7,10 +7,10 @@
 import SwiftUI
 import Flow
 
-/// Displays a read-only collection of landmark categories in a horizontal flow layout
+/// Displays a read-only collection of landmark categories in a flow layout.
 struct CategoriesViewFlow: View {
     
-    /// The categories to update on edit / delete
+    /// The categories to display in the given order.
     @Binding var categories: [LandmarkCategory]
     
     var body: some View {
@@ -18,7 +18,6 @@ struct CategoriesViewFlow: View {
             EmptyView()
         } else {
             HFlow {
-                // TODO patmcg encapsulate this sorting here???
                 ForEach(categories, id: \.id) { category in
                     CategoryCapsuleNew(
                         category: category,
@@ -47,34 +46,6 @@ struct CategoriesViewFlow: View {
     CategoriesViewFlow(categories: $categories)
 }
 
-#Preview("Several - edit") {
-    @Previewable @State var categories: [LandmarkCategory] = [
-        LandmarkCategory(name: "one"),
-        LandmarkCategory(name: "two"),
-        LandmarkCategory(name: "three"),
-        LandmarkCategory(name: "four"),
-        LandmarkCategory(name: "five"),
-        LandmarkCategory(name: "six"),
-        LandmarkCategory(name: "seven"),
-        LandmarkCategory(name: "eight"),
-    ]
-    CategoriesViewFlow(categories: $categories)
-}
-
-#Preview("One") {
-    @Previewable @State var categories: [LandmarkCategory] = [
-        LandmarkCategory(name: "one")
-    ]
-    CategoriesViewFlow(categories: $categories)
-}
-
-#Preview("One - edit") {
-    @Previewable @State var categories: [LandmarkCategory] = [
-        LandmarkCategory(name: "one")
-    ]
-    CategoriesViewFlow(categories: $categories)
-}
-
 #Preview("Two") {
     @Previewable @State var categories: [LandmarkCategory] = [
         LandmarkCategory(name: "one"),
@@ -83,10 +54,9 @@ struct CategoriesViewFlow: View {
     CategoriesViewFlow(categories: $categories)
 }
 
-#Preview("Two - edit") {
+#Preview("One") {
     @Previewable @State var categories: [LandmarkCategory] = [
-        LandmarkCategory(name: "one"),
-        LandmarkCategory(name: "two")
+        LandmarkCategory(name: "one")
     ]
     CategoriesViewFlow(categories: $categories)
 }
