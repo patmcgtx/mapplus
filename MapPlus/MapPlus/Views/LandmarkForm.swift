6f7c7e7f75524703027530a2e3117c15bf7b6f3a
patmcgtx <57506933+patmcgtx@users.noreply.github.com>
2026-04-23 19:00:19 -0500

    Used a more LG-style save button

--- MapPlus/MapPlus/Views/LandmarkForm.swift
+++ MapPlus/MapPlus/Views/LandmarkForm.swift
@@ -214,8 +214,10 @@ struct LandmarkForm: View {
     }
     
     private var saveButton: some View {
-        Button("save".localized) {
+        Button {
             viewModel.save(using: LandmarkStore(modelContext: modelContext))
+        } label: {
+            Image(systemName: "checkmark")
         }
         .disabled(!viewModel.isSaveEnabled)
     }
