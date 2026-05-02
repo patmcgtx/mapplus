9cc4d316bc1a785ebff4068a7f3c5bba0b1932c1
patmcgtx <57506933+patmcgtx@users.noreply.github.com>
2026-05-02 13:02:59 -0500

    #249 Progress on data model

---
+++ MapPlus/MapPlus/Common/CategorySelectionType.swift
@@ -0,0 +1,18 @@
+//
+//  CategorySelectionType.swift
+//  MapPlus
+//
+//  Created by Patrick McGonigle on 5/2/26.
+//
+
+/// How to category selection works.
+enum CategorySelectionType: String, Codable {
+    
+    /// Show landmarks with _any_ of the selected categories.
+    /// For example "Cafe" and "Open Early" means all cafes and all places that are open early.
+    case showLandmarksMatchingAny
+    
+    /// Show landmarks with _all_ of the selected categories.
+    /// For example "Cafe" and "Open Early" means only cafes that are open early.
+    case showLandmarksMatchingAll
+}
---
+++ MapPlus/MapPlus/Persistence/AppSettings.swift
@@ -0,0 +1,32 @@
+//
+//  AppSettings.swift
+//  MapPlus
+//
+//  Created by Patrick McGonigle on 5/2/26.
+//
+import SwiftData
+
+// TODO patmcg next steps:
+// - Add a AppSettingsStoring / AppSettingsStore pattern?  Is it needed?
+// - At least provide an easy way to get the one instance and query and update this thing.
+// - Update it in CategoriesSelection flow with a "match any" / "match all" toggle.
+// - Query it in MainMapView to switch between filteredLandmarksOr / filteredLandmarksAnd
+
+/// Storage model for persistent app settings.
+///
+/// We could be using `@AppStorage` instead, but I'm trying SwiftData for these reasons:
+/// * Some of the user state such as `LandmarkCategory.isSelected` are saved in SwiftData,
+///     so saving any related state the same way provided consistency.  For example the category selection
+///     interacts with `LandmarkCategory.isSelected`, so I want to keep them in the same place to prevent bugs.
+/// * Putting these settings in SwiftData will (potentially) allow cross-device sync of state via iCloud.
+/// * It also goes with my "all in on SwiftData for app state" approach / experiment.
+@Model
+class AppSettings {    
+    
+    /// The current state of category selection.
+    var categorySelectionType = CategorySelectionType.showLandmarksMatchingAny
+    
+    init() {
+        //
+    }
+}
